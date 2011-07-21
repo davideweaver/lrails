1257904eec0fea492aced0a51b4fd1f1ccd8a701
dave <dave@loggr.net>
2011-07-21 16:32:09 -0400

    changes

--- lib/loggr/catcher.rb
+++ lib/loggr/catcher.rb
@@ -3,20 +3,20 @@ module Loggr
     class << self
       def handle_with_controller(exception, controller=nil, request=nil)
         if Config.should_send_to_api?
-		  Loggr::Events.create_from_exception(exception).post()
+		  Loggr::Events.create_from_exception(exception, request).post()
         end
       end
       
       def handle_with_rack(exception, environment, request) 
         if Config.should_send_to_api?
           # data = RackExceptionData.new(exception, environment, request)
-		  Loggr::Events.create_from_exception(exception).post()
+		  Loggr::Events.create_from_exception(exception, request).post()
         end
       end
 
       def handle(exception, name=nil)
         if Config.should_send_to_api?
-          Loggr::Events.create_from_exception(exception).post()
+          Loggr::Events.create_from_exception(exception, request).post()
         end
       end
     end
--- lib/loggr/controller_exception_data.rb
+++ lib/loggr/controller_exception_data.rb
@@ -9,12 +9,18 @@ module Loggr
       res = res + sprintf("<b>Exception</b>: %s<br />", ex.message)
       res = res + sprintf("<b>Type</b>: %s<br />", ex.class)
       res = res + sprintf("<b>Machine</b>: %s<br />", get_hostname)
-      res = res + sprintf("<b>Language</b>: %s<br />", language_version_string)
+      res = res + sprintf("<b>Language</b>: ruby %s<br />", language_version_string)
       res = res + "<br />"
 
 	  # web details
 	  if !request.nil?
-	    res = res + sprintf("<b>Request URL</b>: %s<br />", (@request.respond_to?(:url) ? @request.url : "#{@request.protocol}#{@request.host}#{@request.request_uri}"))
+	    res = res + sprintf("<b>Request URL</b>: %s<br />", (request.respond_to?(:url) ? request.url : "#{request.protocol}#{request.host}#{request.request_uri}"))
+	    res = res + sprintf("<b>User</b>: %s<br />", get_username)
+	    res = res + sprintf("<b>User host address</b>: %s<br />", (request.respond_to?(:remote_ip) ? request.remote_ip : request.ip))
+	    res = res + sprintf("<b>Request Method</b>: %s<br />", request.request_method.to_s)
+	    res = res + sprintf("<b>User Agent</b>: %s<br />", request.env['HTTP_USER_AGENT'] || 'UNKNOWN')
+	    res = res + sprintf("<b>Referer</b>: %s<br />", request.env['HTTP_REFERER'] || 'UNKNOWN')
+	    res = res + sprintf("<b>Script Name</b>: %s<br />", request.env['SCRIPT_NAME'] || 'UNKNOWN')
 	    res = res + "<br />"
 	  end
 
@@ -39,22 +45,6 @@ module Loggr
 
     def self.get_username
       ENV['LOGNAME'] || ENV['USER'] || ENV['USERNAME'] || ENV['APACHE_RUN_USER'] || 'UNKNOWN'
-    end
-
-    def extra_stuff                                                                                               
-      return {} if @request.nil?
-      {
-        'request' => {
-          'url' => (@request.respond_to?(:url) ? @request.url : "#{@request.protocol}#{@request.host}#{@request.request_uri}"),
-          'controller' => @controller.class.to_s,
-          'action' => (@request.respond_to?(:parameters) ? @request.parameters['action'] : @request.params['action']),
-          'parameters' => filter_paramaters(@request.respond_to?(:parameters) ? @request.parameters : @request.params),
-          'request_method' => @request.request_method.to_s,
-          'remote_ip' => (@request.respond_to?(:remote_ip) ? @request.remote_ip : @request.ip),
-          'headers' => extract_http_headers(@request.env),
-          'session' => self.class.sanitize_session(@request)
-        }
-      }
     end
   end
 end
